4ada7ab41add02dbe94ffa2848df5445a7283788
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-10 22:14:24 +0800

    Add files via upload

---
+++ src/GALILEO/Packets/Messages/Server/Battle/Pc_Battle_Data.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using BL.Servers.CoC.Extensions;
+using BL.Servers.CoC.Extensions.List;
+using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Logic.Enums;
+
+namespace BL.Servers.CoC.Packets.Messages.Server.Battle
+{
+    internal class Pc_Battle_Data : Message
+    {
+        internal Level Enemy = null;
+        internal Battle_Mode BattleMode;
+
+        public Pc_Battle_Data(Device Device) : base(Device)
+        {
+            this.Identifier = 24107;
+        }
+
+        internal override void Encode()
+        {
+            this.Enemy.Tick();
+            using (Objects Home = new Objects(Enemy, Enemy.JSON))
+            {
+                this.Data.AddInt((int)(Home.Timestamp - DateTime.UtcNow).TotalSeconds);
+                this.Data.AddInt(-1);
+
+                this.Data.AddRange(Home.ToBytes);
+                this.Data.AddRange(Enemy.Avatar.ToBytes);
+                this.Data.AddRange(this.Device.Player.Avatar.ToBytes);
+
+                this.Data.AddInt((int)this.BattleMode);
+                this.Data.AddInt(0);
+                this.Data.Add(0);
+            }
+        }
+
+        internal override void Process()
+        {
+            this.Device.Player.Avatar.Last_Attack_Enemy_ID.Add((int)this.Enemy.Avatar.UserId);
+
+            if (this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count > 20)
+                this.Device.Player.Avatar.Last_Attack_Enemy_ID.RemoveAt(0);
+            
+            this.Device.State = Logic.Enums.State.IN_PC_BATTLE;
+
+            if (this.Device.Player.Avatar.Battle_ID == 0)
+            {
+                Core.Resources.Battles.New(this.Device.Player, Core.Resources.Players.Get(this.Device.Player.Avatar.Last_Attack_Enemy_ID[this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count - 1], Constants.Database), Constants.Database);
+            }
+            else
+            {
+                Core.Resources.Battles.Save(Core.Resources.Battles.Get(this.Device.Player.Avatar.Battle_ID, Constants.Database, false));
+                Core.Resources.Battles.Remove(this.Device.Player.Avatar.Battle_ID);
+
+                Core.Resources.Battles.New(this.Device.Player, Core.Resources.Players.Get(this.Device.Player.Avatar.Last_Attack_Enemy_ID[this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count - 1], Constants.Database), Constants.Database);
+            }
+        }
+
+    }
+}
---
+++ src/GALILEO/Packets/Messages/Server/Battle/Replay_Data.cs
@@ -0,0 +1,26 @@
+﻿using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Extensions;
+using BL.Servers.CoC.Extensions.List;
+
+namespace BL.Servers.CoC.Packets.Messages.Server.Battle
+{
+    internal class Replay_Data : Message
+    {
+        internal long Battle_ID = 0;
+
+        public Replay_Data(Device _Device) : base(_Device)
+        {
+            this.Identifier = 24114;
+        }
+
+        internal override void Encode()
+        {
+            var Battle = Core.Resources.Battles.Get(this.Battle_ID, Constants.Database, false);
+
+            if (Battle != null)
+            {
+                this.Data.AddCompressed(Battle.Json, false);
+            }
+        }
+    }
+}
