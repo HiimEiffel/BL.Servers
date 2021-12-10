42888d76525a8bc90f37c7b247c351e2fd0752f9
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-10 23:32:58 +0800

    Create Column.cs

---
+++ src/GALILEO/Files/CSV_Reader/Column.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Servers.CoC.Files.CSV_Reader
+{
+
+    [DebuggerDisplay("Name = {Name}")]
+    public class Column
+    {
+        public Column(Table table, string name)
+        {
+            _name = name;
+            _table = table ?? throw new ArgumentNullException(nameof(table));
+            _table._columns.Add(this);
+
+            _data = new List<string>();
+        }
+
+        internal readonly List<string> _data;
+
+        private readonly string _name;
+        private readonly Table _table; public Table Table => _table;
+
+        public string Name => _name;
+    }
+}
