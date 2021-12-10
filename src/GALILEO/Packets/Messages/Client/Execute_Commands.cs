a0e421c9634e2b034781be8ec13445d8fa80092a
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-10 21:32:13 +0800

    Add files via upload

---
+++ src/GALILEO/Packets/Messages/Client/Execute_Commands.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Servers.CoC.Core;
+using BL.Servers.CoC.Extensions;
+using BL.Servers.CoC.Extensions.Binary;
+using BL.Servers.CoC.Logic;
+
+namespace BL.Servers.CoC.Packets.Messages.Client
+{
+    internal class Execute_Commands : Message
+    {
+        internal int CTick;
+        internal int STick;
+        internal uint Checksum;
+        internal int Count;
+
+        internal byte[] Commands;
+        internal List<Command> LCommands;
+
+
+        public Execute_Commands(Device Device, Reader Reader) : base(Device, Reader)
+        {
+        }
+
+        internal override void Decode()
+        {
+            this.CTick = this.Reader.ReadInt32();
+            this.Checksum = this.Reader.ReadUInt32();
+            this.Count = this.Reader.ReadInt32();
+
+            this.STick += (int)Math.Floor(DateTime.UtcNow.Subtract(this.Device.Player.Avatar.LastTick).TotalSeconds * 20);
+            this.LCommands = new List<Command>((int)this.Count);
+            this.Commands = this.Reader.ReadBytes((int)(this.Reader.BaseStream.Length - this.Reader.BaseStream.Position));
+        }
+
+        internal override void Process()
+        {
+            if (this.Device.State == Logic.Enums.State.IN_PC_BATTLE)
+                Resources.Battles.Get(this.Device.Player.Avatar.Battle_ID, Constants.Database).Battle_Tick = (int)this.CTick;
+            if (this.Count > -1 && this.Count <= 400)
+            {
+                this.Device.Player.Tick();
+                using (Reader Reader = new Reader(this.Commands))
+                {
+                    for (int _Index = 0; _Index < this.Count; _Index++)
+                    {
+                        int CommandID = Reader.ReadInt32();
+                        if (CommandFactory.Commands.ContainsKey(CommandID))
+                        {
+                            Command Command = Activator.CreateInstance(CommandFactory.Commands[CommandID], Reader, this.Device, CommandID) as Command;
+
+                            if (Command != null)
+                            {
+#if DEBUG
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine("Command " + CommandID + " has  been handled.");
+                                Console.ResetColor();
+#endif
+                                Command.Decode();
+                                Command.Process();
+
+                                this.LCommands.Add(Command);
+                            }
+                        }
+                        else
+                        {
+#if DEBUG
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Command " + CommandID + " has not been handled.");
+                            if (this.LCommands.Any()) Console.WriteLine("Previous command was " + this.LCommands.Last().Identifier + ". [" + (_Index + 1) + " / " + this.Count + "]");
+                            Console.ResetColor();
+                            break;
+#endif
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //new OutOfSyncMessage(this.Device).Send();
+            }
+        }
+    }
+}
---
+++ src/GALILEO/Packets/Messages/Client/Go_Home.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Servers.CoC.Core;
+using BL.Servers.CoC.Core.Database;
+using BL.Servers.CoC.Core.Networking;
+using BL.Servers.CoC.Extensions;
+using BL.Servers.CoC.Extensions.Binary;
+using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Logic.Structure.Slots.Items;
+using BL.Servers.CoC.Packets.Messages.Server;
+
+namespace BL.Servers.CoC.Packets.Messages.Client
+{
+    internal class Go_Home : Message
+    {
+        internal int State;
+
+        public Go_Home(Device Device, Reader Reader) : base(Device, Reader)
+        {
+            // Go_Home.
+        }
+
+        internal override void Decode()
+        {
+            this.State = this.Reader.ReadInt32();
+            this.Reader.ReadByte();
+        }
+
+        internal override void Process()
+        {
+
+            if (State == 1)
+            {
+                this.Device.State = Logic.Enums.State.WAR_EMODE;
+            }
+            else
+            {
+                if (this.Device.State == Logic.Enums.State.IN_PC_BATTLE)
+                {
+                    if (this.Device.Player.Avatar.Battle_ID > 0)
+                    {
+                        var Battle =
+                            Core.Resources.Battles.Get(this.Device.Player.Avatar.Battle_ID, Constants.Database);
+                        if (Battle.Commands.Count > 0)
+                        {
+                            Level Player =  Core.Resources.Players.Get(Battle.Defender.UserId, Constants.Database, false);
+
+                            if (Utils.IsOdd(Resources.Random.Next(1, 1000)))
+                            {
+                                int lost = (int)Battle.LoseTrophies();
+                                Player.Avatar.Trophies += (int)Battle.WinTrophies();
+
+                                if (this.Device.Player.Avatar.Trophies >= lost)
+                                    this.Device.Player.Avatar.Trophies -= (int) Battle.LoseTrophies();
+                                else
+                                    this.Device.Player.Avatar.Trophies = 0;
+                            }
+                            else
+                            {
+                                int lost = (int)Battle.LoseTrophies();
+                                if (Player.Avatar.Trophies >= lost)
+                                    Player.Avatar.Trophies -= (int)Battle.LoseTrophies();
+                                else
+                                    Player.Avatar.Trophies = 0;
+
+                                this.Device.Player.Avatar.Trophies += (int)Battle.WinTrophies();
+                            }
+
+                            Battle.WinTrophies();
+                            Battle.LoseTrophies();
+                            Battle.Set_Replay_Info();
+                            this.Device.Player.Avatar.Inbox.Add(
+                                new Mail
+                                {
+                                    Stream_Type = Logic.Enums.Avatar_Stream.ATTACK,
+                                    Battle_ID = this.Device.Player.Avatar.Battle_ID
+                                });
+
+                            //if (Core.Resources.Players.Get(Battle.Defender.UserId, Constants.Database) == null)
+                            {
+                                //if (Player.Avatar.Guard < 1)
+                                Player.Avatar.Inbox.Add(
+                                    new Mail
+                                    {
+                                        Stream_Type = Logic.Enums.Avatar_Stream.DEFENSE,
+                                        Battle_ID = this.Device.Player.Avatar.Battle_ID
+                                    });
+                            }
+                            Core.Resources.Battles.Save(Battle);
+                        }
+                        else
+                            Core.Resources.Battles.Remove(this.Device.Player.Avatar.Battle_ID);
+                        this.Device.Player.Avatar.Battle_ID = 0;
+                        this.Device.State = Logic.Enums.State.LOGGED;
+                    }
+                }
+                else if (this.Device.State == Logic.Enums.State.IN_AMICAL_BATTLE)
+                {
+                    var Alliance = Resources.Clans.Get(this.Device.Player.Avatar.ClanId, Constants.Database, false);
+                    Entry Stream = Alliance.Chats.Get(this.Device.Player.Avatar.Amical_ID);
+                    if (Stream != null)
+                    {
+                        Alliance.Chats.Remove(Stream);
+                    }
+                }
+
+                new Own_Home_Data(this.Device).Send();
+
+                if (this.Device.Player.Avatar.ClanId > 0)
+                {
+                }
+            }
+        }
+    }
+}
