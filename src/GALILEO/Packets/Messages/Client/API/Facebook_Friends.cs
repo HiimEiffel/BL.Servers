688e042b239ba50eccf9e94919312cd3e73ad687
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-10 21:34:41 +0800

    Add files via upload

---
+++ src/GALILEO/Packets/Messages/Client/API/Facebook_Friends.cs
@@ -0,0 +1,37 @@
+﻿using BL.Servers.CoC.Core.Networking;
+using BL.Servers.CoC.Extensions.Binary;
+using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Packets.Messages.Server.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Servers.CoC.Packets.Messages.Client.API
+{
+    internal class Facebook_Friends : Message
+    {
+        internal List<string> Friends;
+
+        public Facebook_Friends(Device Device, Reader Reader) : base(Device, Reader)
+        {
+            // Facebook_Friends.
+        }
+
+        internal override void Decode()
+        {
+            int Count = this.Reader.ReadInt32();
+            this.Friends = new List<string>(Count);
+
+            for (int i = 0; i < Count; i++)
+            {
+                this.Friends.Add(this.Reader.ReadString());
+            }
+        }
+        internal override void Process()
+        {
+            new Friend_List_Data(this.Device, this.Friends).Send();
+        }
+    }
+}
