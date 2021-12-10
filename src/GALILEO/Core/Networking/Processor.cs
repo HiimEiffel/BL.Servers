ac44fc2f95e5754285f8f1ea16bc629b895d1509
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-11 00:09:54 +0800

    Add files via upload

---
+++ src/GALILEO/Core/Networking/Processor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Servers.CoC.Extensions;
+using BL.Servers.CoC.Packets;
+
+namespace BL.Servers.CoC.Core.Networking
+{
+    internal static class Processor
+    {
+       /* internal static void Recept(this Message Message)
+        {
+            Message.Decryptpe();
+            Message.Decode();
+            Message.Process();
+        }*/
+
+        internal static void Send(this Message Message)
+        {
+            try
+            {
+
+                Message.Encode();
+
+#if DEBUG
+                Loggers.Log(Message, Utils.Padding(Message.Device.Socket.RemoteEndPoint.ToString(), 15));
+#endif
+                if (Constants.RC4)
+                    Message.EncryptRC4();
+                else
+                    Message.EncryptPepper();
+
+                Resources.Gateway.Send(Message);
+#if DEBUG
+                if (Message.Device.Connected())
+                {
+                    Console.WriteLine(Utils.Padding(Message.Device.Socket.RemoteEndPoint.ToString(), 15) + " <-- " +
+                                      Message.GetType().Name);
+                }
+#endif
+                Message.Process();
+            }
+            catch (Exception)
+            {
+                //
+            }
+        }
+
+        internal static Command Handle(this Command Command)
+        {
+            Command.Encode();
+
+            return Command;
+        }
+    }
+}
