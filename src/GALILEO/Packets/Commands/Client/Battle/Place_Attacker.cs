4450ed8a46ef8002b1b694eb950ba44d8350e87b
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-10 21:18:33 +0800

    Add files via upload

---
+++ src/GALILEO/Packets/Commands/Client/Battle/Place_Attacker.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using BL.Servers.CoC.Extensions;
+using BL.Servers.CoC.Extensions.Binary;
+using BL.Servers.CoC.Files;
+using BL.Servers.CoC.Files.CSV_Logic;
+using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Logic.Enums;
+using BL.Servers.CoC.Logic.Structure.Slots.Items;
+
+namespace BL.Servers.CoC.Packets.Commands.Client.Battle
+{
+    internal class Place_Attacker : Command
+    {
+
+        internal Characters Troop;
+        internal Spells Spell;
+
+        internal int GlobalId;
+        internal int X;
+        internal int Y;
+        internal int Tick;
+
+        internal bool IsSpell;
+
+        public Place_Attacker(Reader reader, Device client, int id) : base(reader, client, id)
+        {
+        }
+
+        internal override void Decode()
+        {
+            this.X = this.Reader.ReadInt32();
+            this.Y = this.Reader.ReadInt32();
+            this.GlobalId = this.Reader.ReadInt32();
+            if (GlobalId < 4000000)
+            {
+                this.IsSpell = true;
+                this.Spell = CSV.Tables.Get(Gamefile.Spells).GetDataWithID(GlobalId) as Spells;
+            }
+            else
+            {
+                this.Troop = CSV.Tables.Get(Gamefile.Characters).GetDataWithID(GlobalId) as Characters;
+            }
+            this.Tick = this.Reader.ReadInt32();
+        }
+
+        internal override void Process()
+        {
+            if (this.Device.State == State.IN_PC_BATTLE)
+            {
+                var Battle = Core.Resources.Battles.Get(this.Device.Player.Avatar.Battle_ID, Constants.Database);
+                Battle_Command Command = new Battle_Command { Command_Type = this.Identifier, Command_Base = new Command_Base { Base = new Base { Tick = this.Tick }, Data = this.GlobalId, X = this.X, Y = this.Y } };
+                Battle.Add_Command(Command);
+
+                if (IsSpell)
+                {
+                    int Index = Battle.Replay_Info.Spells.FindIndex(T => T[0] == this.GlobalId);
+                    if (Index > -1)
+                        Battle.Replay_Info.Spells[Index][1]++;
+                    else
+                        Battle.Replay_Info.Spells.Add(new[] { this.GlobalId, 1 });
+
+                    Battle.Attacker.Add_Spells(GlobalId, 1);
+                }
+                else
+                {
+                    int Index = Battle.Replay_Info.Units.FindIndex(T => T[0] == this.GlobalId);
+                    if (Index > -1)
+                        Battle.Replay_Info.Units[Index][1]++;
+                    else
+                        Battle.Replay_Info.Units.Add(new[] { this.GlobalId, 1 });
+
+                    Battle.Attacker.Add_Unit(GlobalId, 1);
+                }
+            }
+
+            if (IsSpell)
+            {
+                List<Slot> _PlayerSpells= this.Device.Player.Avatar.Spells;
+
+                Slot _DataSlot = _PlayerSpells.Find(t => t.Data == GlobalId);
+                if (_DataSlot != null)
+                {
+                    _DataSlot.Count -= 1;
+                }
+            }
+            else
+            {
+                List<Slot> _PlayerUnits= this.Device.Player.Avatar.Units;
+
+                Slot _DataSlot = _PlayerUnits.Find(t => t.Data == GlobalId);
+                if (_DataSlot != null)
+                {
+                    _DataSlot.Count -= 1;
+                }
+            }
+
+            /*            List<Component> components = level.GetComponentManager().GetComponents(0);
+            for (int i = 0; i < components.Count; i++)
+            {
+                UnitStorageComponent c = (UnitStorageComponent)components[i];
+                if (c.GetUnitTypeIndex(Unit) != -1)
+                {
+                    var storageCount = c.GetUnitCountByData(Unit);
+                    if (storageCount >= 1)
+                    {
+                        c.RemoveUnits(Unit, 1);
+                        break;
+                    }
+                }
+            }*/
+        }
+    }
+}
---
+++ src/GALILEO/Packets/Commands/Client/Battle/Search_Opponent.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using BL.Servers.CoC.Core.Networking;
+using BL.Servers.CoC.Extensions;
+using BL.Servers.CoC.Extensions.Binary;
+using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Logic.Enums;
+using BL.Servers.CoC.Packets.Messages.Server.Battle;
+using BL.Servers.CoC.Packets.Messages.Server.Errors;
+
+namespace BL.Servers.CoC.Packets.Commands.Client.Battle
+{
+    internal class Search_Opponent : Command
+    {
+        internal long Enemy_ID;
+        internal bool Max_Seed_Achieved;
+        internal Level Enemy_Player;
+        public Search_Opponent(Reader _Reader, Device _Client, int _ID) : base(_Reader, _Client, _ID)
+        {
+        }
+
+        internal override void Decode()
+        {
+            this.Reader.ReadInt32();
+            this.Reader.ReadInt32();
+            this.Reader.ReadInt32();
+        }
+
+        internal override void Process()
+        {
+            if (this.Device.State == Logic.Enums.State.LOGGED)
+            {
+                this.Device.Player.Tick();
+            }
+
+            this.Device.State = Logic.Enums.State.SEARCH_BATTLE;
+            if (this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count > 20 ||
+                this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count == (Core.Resources.Players.Seed - 2))
+            {
+                if (this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count != 0)
+                {
+                    this.Device.Player.Avatar.Last_Attack_Enemy_ID.RemoveAt(0);
+                }
+            }
+            while (this.Enemy_Player == null && this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count < Core.Resources.Players.Seed - 2)
+            {
+
+                if (this.Enemy_ID != this.Device.Player.Avatar.UserId && this.Enemy_ID > 0)
+                {
+                    if (this.Device.Player.Avatar.Last_Attack_Enemy_ID.FindIndex(P => P == this.Enemy_ID) < 0)
+                    {
+                        this.Enemy_Player = Core.Resources.Players.Get(this.Enemy_ID, Constants.Database, false);
+
+                        if (this.Enemy_Player == null)
+                        {
+                            this.Device.Player.Avatar.Last_Attack_Enemy_ID.Add(Enemy_ID);
+
+                            if (this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count > 20 || this.Device.Player.Avatar.Last_Attack_Enemy_ID.Count > Core.Resources.Players.Seed - 2)
+                                this.Device.Player.Avatar.Last_Attack_Enemy_ID.RemoveAt(0);
+                        }
+                    }
+                    else
+                    {
+                        if (this.Enemy_ID < Core.Resources.Players.Seed - 1 && !this.Max_Seed_Achieved)
+                        {
+                            this.Enemy_ID++;
+                        }
+                        else
+                        {
+                            if (this.Enemy_ID < 1) break;
+                            this.Enemy_ID--;
+                            this.Max_Seed_Achieved = true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (this.Enemy_ID < Core.Resources.Players.Seed - 1 && !this.Max_Seed_Achieved)
+                    {
+                        this.Enemy_ID++;
+                    }
+                    else
+                    {
+                        if (this.Enemy_ID < 1)
+                        {
+                            break;
+                        }
+                        this.Enemy_ID--;
+                        this.Max_Seed_Achieved = true;
+                    }
+                }
+            }
+
+            if (this.Enemy_Player != null)
+                new Pc_Battle_Data(this.Device) { Enemy = this.Device.Player,  BattleMode = Battle_Mode.PVP}.Send();
+            else
+                new Battle_Failed(this.Device).Send();
+        }
+    }
+}
