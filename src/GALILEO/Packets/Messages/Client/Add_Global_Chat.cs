c067bbfca17b5cf2fa4515af4dcb30b15b797d91
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-10 21:29:20 +0800

    Create Add_Global_Chat.cs

---
+++ src/GALILEO/Packets/Messages/Client/Add_Global_Chat.cs
@@ -0,0 +1,30 @@
+using BL.Servers.CoC.Core;
+using BL.Servers.CoC.Core.Networking;
+using BL.Servers.CoC.Extensions.Binary;
+using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Packets.Messages.Server;
+
+namespace BL.Servers.CoC.Packets.Messages.Client
+{
+    internal class Add_Global_Chat : Message 
+    {
+        internal string Message = string.Empty;
+        
+        public Add_Global_Chat(Device Device, Reader Reader) : base(Device, Reader)
+        {
+        }
+        
+        internal override void Decode()
+        {
+            this.Message = this.Reader.ReadString();
+        }
+        
+        internal override void Process()
+        {
+            foreach (var _Device in Resources.GChat.Get_Chat(this.Device).Values)
+            {
+              new Global_Chat_Entry (_Device){ Message = this.Message, Message_Sender = this.Device.Player.Avatar, Sender = this.Device == _Device }.Send();
+            }
+        }
+    }
+}
