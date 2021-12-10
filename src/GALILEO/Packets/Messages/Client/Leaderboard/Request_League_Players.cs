f2f32668cc0d9ecdb8aad18a66dc41a39992bac2
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-10 22:00:59 +0800

    Add files via upload

---
+++ src/GALILEO/Packets/Messages/Client/Leaderboard/Request_League_Players.cs
@@ -0,0 +1,23 @@
+﻿using BL.Servers.CoC.Core.Networking;
+using BL.Servers.CoC.Extensions.Binary;
+using BL.Servers.CoC.Logic;
+using BL.Servers.CoC.Packets.Messages.Server.Leaderboard;
+
+namespace BL.Servers.CoC.Packets.Messages.Client.Leaderboard
+{
+    internal class Request_League_Players : Message
+    { 
+        public Request_League_Players(Device Device, Reader Reader) : base(Device, Reader)
+        {
+        }
+
+        internal override void Decode()
+        {
+        }
+
+        internal override void Process()
+        {
+            new League_Players(this.Device).Send();
+        }
+    }
+}
